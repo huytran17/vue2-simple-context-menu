60264612995adaee5b23f220cb65ee9cb34f07ab
huytran17 <alpha.lloyd1368@gmail.com>
2022-11-30 20:06:30 +0700

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { Vue2SimpleContextMenuSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  Vue2SimpleContextMenuSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue2-simple-context-menu-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as Vue2SimpleContextMenuSample } from './vue2-simple-context-menu-sample.vue';
